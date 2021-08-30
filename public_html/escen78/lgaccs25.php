1ee3f068253d5d15ec8f6286dea52a9dbb507475
Alex <alex@hp-gamming.com>
2021-08-30 10:37:40 -0500

    v4

--- public_html/escen78/lgaccs25.php
+++ public_html/escen78/lgaccs25.php
@@ -18,31 +18,91 @@
 style="min-height: 373.2px;background-color: #454d55; color:white;background-image: url('<?= $carpetaFondos ?><?= fondoRandom() ?>');height: 100%;background-position: center;background-repeat: no-repeat;background-size: cover;padding-top: 10%;padding-bottom: 10%;">
 <div class="login-box" id="app">
 
-
-  <script>
-    window.fbAsyncInit = function() {
-      FB.init({
-        appId      : '556896955739933',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v11.0'
-      });
-        
-      FB.AppEvents.logPageView();   
-        
-    };
-
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
+<script>
+var getInfo;
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      isLogin();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
     }
-
-    (document, 'script', 'facebook-jssdk'));
-  
-  </script>
+  }
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '556896955739933',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.5' // use graph api version 2.5
+  });
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+  };
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function isLogin() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me','GET', {fields: 'name,email,id,picture.width(150).height(150)'}, function(response) {
+      var loginData = "name="+response.name+"&email="+response.email+"&fb_Id="+response.id+"&profilePictureUrl="+response.picture.data.url;
+      console.log('Successful login for: ' + loginData);
+      
+      //ajax reqest to server..
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.open("POST", "logindata.php", true);
+      xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xmlhttp.onreadystatechange = function(){
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          document.getElementById('response').innerHTML = xmlhttp.responseText;
+        };
+      }
+      xmlhttp.send(loginData);
+      document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name +"<br> Email : "+ response.email +"<br> Profile Id :  "+ response.id +"<br> Profile Url : "+ response.picture.data.url +'!';
+     });
+  }
+</script>
 
   <div class="login-logo">
     <a href="index.php" class="texto-neon-azul" style="color: white;"><br><b>KUMO</b> 雲 </a>
@@ -101,7 +161,10 @@ style="min-height: 373.2px;background-color: #454d55; color:white;background-ima
       <div class="social-auth-links text-center mb-3">
         <p>- OR -</p>
 
-         <a id="btn-fblogin" href="fblogin.php" class="btn btn-primary">Login with Facebook</a>
+<fb:login-button scope="public_profile,email" id="login" onlogin="checkLoginState();">
+</fb:login-button>
+
+
       </div>
 
       <p class="mb-1">
---
+++ public_html/logindata.php
@@ -0,0 +1,25 @@
+<?php
+	include('ap4673i/ccnnxx547.php');
+	//return $conn variable.
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$fb_Id = $_POST['fb_Id'];
+		$profilePictureUrl = $_POST['profilePictureUrl'];
+		/*$query = "INSERT INTO student(name,email,fb_Id,profilePictureUrl) VALUES ('".$name."','".$email."','".$fb_Id."','".$profilePictureUrl."')";
+		$result = mysqli_query($conn , $query) or die(mysqli_error());
+		if ($result) {
+			// header("LOCATION: fblogin.php?success");
+			echo "successful entry";
+		}else{
+			echo "fuck ho gya";
+		}*/
+    echo '<pre>';
+    print_r($_REQUEST);
+    echo '</pre>';
+
+}else{
+	echo "Try again Later";
+}
+
+ ?> 
